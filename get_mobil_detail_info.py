571e1175f9152dacb64cd6a6d7d9b35196949ad3
yidun55 <heshang1203@sina.com>
2015-07-07 17:41:35 +0800

    second

--- get_mobil_detail_info.py
+++ get_mobil_detail_info.py
@@ -6,6 +6,7 @@ import urllib2
 import HTMLParser
 import logging
 from logging import handlers
+import time
 
 LOG_FILE = "mobile_num.log"
 logger = logging.getLogger()
@@ -55,49 +56,54 @@ def crawl_mobile_info():
     
     f = open("norecord_mobile_code.txt")
     for mNum in f.readlines():
-        detail_list = [mNum.strip()]
-        #以下是抓取shouji上的数据
-        base_url_shouji = 'http://shouji.supfree.net/fish.asp?cat=%s'
-        url = base_url_shouji %str(mNum).rstrip()
-        xtml = urllib2.urlopen(url)
-        content = xtml.read()
-        content = content.decode("gb2312").encode("utf-8")
-        content = bytes(content)
-        sel = Selector(text=content, type='html')
-        classi = sel.xpath(u"//span[text()='卡类型：']/../text()").extract()
-        detail_list = for_ominated_data(detail_list, classi)
-        bus = sel.xpath(u"//span[text()='运营商：']/following-sibling::a[1]/text()").extract() #运营商
-        detail_list = for_ominated_data(detail_list,bus)
-
-        #以下是从ip138上抓取
-        base_url_ip138 = 'http://www.ip138.com:8080/search.asp?mobile=%s&action=mobile'
-        url = base_url_ip138 %str(mNum).rstrip()
-        xtml = urllib2.urlopen(url)
-        content = xtml.read()
-        content = content.decode("gb2312").encode("utf-8")
-        content = bytes(content)
-        sel = Selector(text=content, type='html')
-#            html_parse = HTMLParser.HTMLParser()
-#            clue = '卡&nbsp;类&nbsp;型'
-#            newclue = html_parse.unescape(clue)  #解析html中的空格
-#            xpath_syn = u"//td[text()='%s']/following-sibling::td[1]/text()"%newclue
-#            classi = sel.xpath(xpath_syn).extract()  #电话卡类型
-#            detail_list = for_ominated_data(detail_list,classi)
-        district_code = sel.xpath(u"//td[text()='区 号']/following-sibling::td[1]/text()").extract()
-        detail_list = for_ominated_data(detail_list,district_code)   #区号
-        fox_code = sel.xpath(u"//td[text()='邮 编']/following-sibling::td[1]/text()").extract()
-        detail_list = for_ominated_data(detail_list,fox_code)   #邮编
-        local = sel.xpath(u"//td[text()='卡号归属地']/following-sibling::td[1]/text()").extract()
-        html_parse = HTMLParser.HTMLParser()
-        #local = html_parse.unescape(local)  #解析html中的空格
         try:
-            local = local[0].strip()
-            local = local.split(html_parse.unescape("&nbsp;")) #应老马要求将归属地拆开
+            detail_list = [mNum.strip()]
+            #以下是抓取shouji上的数据
+            base_url_shouji = 'http://shouji.supfree.net/fish.asp?cat=%s'
+            url = base_url_shouji %str(mNum).rstrip()
+            xtml = urllib2.urlopen(url)
+            content = xtml.read()
+            content = content.decode("gb2312").encode("utf-8")
+            content = bytes(content)
+            sel = Selector(text=content, type='html')
+            classi = sel.xpath(u"//span[text()='卡类型：']/../text()").extract()
+            detail_list = for_ominated_data(detail_list, classi)
+            bus = sel.xpath(u"//span[text()='运营商：']/following-sibling::a[1]/text()").extract() #运营商
+            detail_list = for_ominated_data(detail_list,bus)
+
+            #以下是从ip138上抓取
+            base_url_ip138 = 'http://www.ip138.com:8080/search.asp?mobile=%s&action=mobile'
+            url = base_url_ip138 %str(mNum).rstrip()
+            xtml = urllib2.urlopen(url)
+            content = xtml.read()
+            content = content.decode("gb2312").encode("utf-8")
+            content = bytes(content)
+            sel = Selector(text=content, type='html')
+    #            html_parse = HTMLParser.HTMLParser()
+    #            clue = '卡&nbsp;类&nbsp;型'
+    #            newclue = html_parse.unescape(clue)  #解析html中的空格
+    #            xpath_syn = u"//td[text()='%s']/following-sibling::td[1]/text()"%newclue
+    #            classi = sel.xpath(xpath_syn).extract()  #电话卡类型
+    #            detail_list = for_ominated_data(detail_list,classi)
+            district_code = sel.xpath(u"//td[text()='区 号']/following-sibling::td[1]/text()").extract()
+            detail_list = for_ominated_data(detail_list,district_code)   #区号
+            fox_code = sel.xpath(u"//td[text()='邮 编']/following-sibling::td[1]/text()").extract()
+            detail_list = for_ominated_data(detail_list,fox_code)   #邮编
+            local = sel.xpath(u"//td[text()='卡号归属地']/following-sibling::td[1]/text()").extract()
+            html_parse = HTMLParser.HTMLParser()
+            #local = html_parse.unescape(local)  #解析html中的空格
+            try:
+                local = local[0].strip()
+                local = local.split(html_parse.unescape("&nbsp;")) #应老马要求将归属地拆开
+            except Exception,e:
+                setLog("归属地_errror %"%"|".join(detail_list),) 
+            detail_list = for_ominated_data(detail_list, local)#卡号归属地
+            in_f = open("detail_info_norecord_mobile_num.txt","a")
+            in_f.write("|".join(detail_list)+"\n")
+            time.sleep(1)
         except Exception,e:
-            setLog("归属地_errror %"%"|".join(detail_list),) 
-        detail_list = for_ominated_data(detail_list, local)#卡号归属地
-        in_f = open("detail_info_norecord_mobile_num.txt","a")
-        in_f.write("|".join(detail_list)+"\n")
+            setLog("error mobile_num=%s" %mNum) 
+            
 
 
 crawl_mobile_info()
